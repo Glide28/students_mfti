3a5c9fb63b50b3fe3715998f15e5f41870d54bd9
Glide28 <51880577+Glide28@users.noreply.github.com>
2025-12-07 10:33:08 +0300

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -4,18 +4,15 @@ from pydantic import BaseModel
 
 
 # ====== Student схемы ======
-
 class StudentBase(BaseModel):
     name: str
     faculty: str
     course: str
     score: int
 
-
 class StudentCreate(StudentBase):
     """Схема для создания студента."""
     pass
-
 
 class StudentUpdate(BaseModel):
     """Схема для частичного обновления студента."""
@@ -24,31 +21,24 @@ class StudentUpdate(BaseModel):
     course: Optional[str] = None
     score: Optional[int] = None
 
-
 class StudentOut(StudentBase):
     """Схема для возврата студента клиенту."""
     id: int
-
     class Config:
         from_attributes = True
 
-
 # ====== Auth схемы ======
-
 class RegisterData(BaseModel):
     username: str
     password: str
-
 
 class LoginData(BaseModel):
     username: str
     password: str
 
-
 class UserOut(BaseModel):
     id: int
     username: str
     token: str
-
     class Config:
         from_attributes = True
